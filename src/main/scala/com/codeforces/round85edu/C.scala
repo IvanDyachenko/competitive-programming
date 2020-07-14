d2644e358b314fb7583ad69d05b9eb0aa5fb0a35
Ivan <vandyachen@gmail.com>
2020-07-15 02:42:02 +0300

    Educational Codeforces Round 85 (Rated for Div. 2)
    https://codeforces.com/contest/1334
    
    - [TIME_LIMIT_EXCEEDED] C. Circle of Monsters, https://codeforces.com/contest/1334/problem/C

--- src/main/scala/com/codeforces/round85edu/C.scala
+++ src/main/scala/com/codeforces/round85edu/C.scala
@@ -7,23 +7,26 @@ package com.codeforces.round85edu
 object C extends App {
   import scala.io.StdIn._
 
-  case class Monster(h: Long, e: Long)
+  var hs = Array.ofDim[Long](300000)
+  var es = Array.ofDim[Long](300000)
 
   val t = readInt()
 
   (0 until t).foreach { _ =>
     val n = readInt()
-    val mn = (0 until n).foldLeft(Array.ofDim[Monster](n)) { (ms, i) =>
+
+    (0 until n).foreach { i =>
       val Array(h, e) = readLine().split(" ").map(_.toLong)
-      ms(i) = Monster(h, e)
-      ms
+      hs(i) = h
+      es(i) = e
     }
 
-    val (bs, b) = mn.indices.foldLeft((0L, Long.MaxValue)) {
+    val (bs, b) = (0 until n).foldLeft((0L, Long.MaxValue)) {
       case ((bs, b), i) =>
-        val j = (i + 1) % n
+        val j = if (i + 1 == n) 0 else i + 1
+        val a = hs(j) min es(i)
 
-        (bs + (mn(j).h - mn(i).e).max(0L), b.min(mn(j).h min mn(i).e))
+        (bs + hs(j) - a, b min a)
     }
 
     val ans = bs + b

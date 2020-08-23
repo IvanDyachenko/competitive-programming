7188916cb21ddc9a669fbfa9ae81565ab0af7adc
Ivan Dyachenko <vandyachen@gmail.com>
2020-08-24 02:54:31 +0300

    Clean up .envrc and shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { }, ... }:
 
-with pkgs;
-
-mkShell {
-  buildInputs = [ metals-emacs ];
+pkgs.stdenv.mkDerivation rec {
+  name = "development-env";
+  env = pkgs.buildEnv { name = name; paths = buildInputs; };
+  buildInputs =
+    let
+      texlive-custom = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-basic
+          # Needed on top of scheme-basic
+          ulem capt-of wrapfig metafont;
+      };
+    in
+      [
+        # Development
+        pkgs.metals-emacs
+        
+        # Documentation
+        pkgs.ditaa texlive-custom
+      ];
 }

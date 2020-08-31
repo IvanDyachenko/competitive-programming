be09272395ad3d48b2300dc6b427f41f825824f2
Ivan Dyachenko <vandyachen@gmail.com>
2020-09-01 00:30:01 +0300

    ITMO Academy: pilot course » Suffix Array » Step 2 ITMO Academy: pilot course » Suffix Array » Step 2 » Practice
    Improved algorithm
    - Using Radix Sort
    - Additional optimizations

--- codeforces/src/main/scala/com/codeforces/edu/suffix_array/SuffixArray.scala
+++ codeforces/src/main/scala/com/codeforces/edu/suffix_array/SuffixArray.scala
@@ -1,8 +1,88 @@
 package com.codeforces.edu.suffix_array
 
+import scala.collection.mutable
+
 object SuffixArray {
 
-  def suffixes(str: String): Seq[String] = {
+  /**
+    * A. Suffix Array - 2
+    * https://codeforces.com/edu/course/2/lesson/2/2/practice/contest/269103/problem/A
+    * https://codeforces.com/edu/course/2/lesson/2/2/practice/contest/269103/submission/91524841
+    */
+  def suffixes2(str: String): Seq[String] = {
+    val s = str + "$"
+    val n = s.length
+
+    var ps = Array.ofDim[Int](n)
+    var cs = Array.ofDim[Int](n)
+
+    s.zipWithIndex.sortBy(_._1).zipWithIndex.foldLeft('$') {
+      case (_, ((curr, p), 0)) =>
+        ps(0) = p
+        cs(p) = 0
+        curr
+      case (prev, ((curr, p), i)) =>
+        ps(i) = p
+        cs(p) = cs(ps(i - 1)) + (if (curr == prev) 0 else 1)
+        curr
+    }
+
+    /**
+      * Count sort
+      */
+    def sort(ps: IndexedSeq[Int]): IndexedSeq[Int] = {
+      var is = cs
+        .foldLeft(Array.fill[Int](cs.max + 1)(0)) {
+          case (is, c) =>
+            is(c) += 1
+            is
+        }
+        .scanLeft(0)(_ + _)
+
+      var sp = Array.ofDim[Int](n)
+      ps.foreach { p =>
+        val c = cs(p)
+        sp(is(c)) = p
+        is(c) += 1
+      }
+
+      sp
+    }
+
+    @annotation.tailrec
+    def go(iter: Int): Seq[String] =
+      if ((1 << iter) > n) ps.map(p => str.slice(p, n))
+      else {
+        ps.indices.foreach(i => ps(i) = (ps(i) - (1 << iter) + n) % n)
+
+        ps = sort(ps).toArray
+
+        var sc = Array.ofDim[Int](n)
+        ps.indices.foreach {
+          case 0 => cs(ps(0)) = 0
+          case i =>
+            // format: off
+            val prev = cs(ps(i - 1)) -> cs((ps(i - 1) + (1 << iter)) % n)
+            val curr = cs(ps(i))     -> cs((ps(i)     + (1 << iter)) % n)
+            // format: on
+
+            sc(ps(i)) = sc(ps(i - 1)) + (if (curr == prev) 0 else 1)
+        }
+
+        cs = sc
+
+        go(iter + 1)
+      }
+
+    go(0)
+  }
+
+  /**
+    * A. Suffix Array - 1
+    * https://codeforces.com/edu/course/2/lesson/2/1/practice/contest/269100/problem/A
+    * https://codeforces.com/edu/course/2/lesson/2/1/practice/contest/269100/submission/91002753
+    */
+  def suffixes1(str: String): Seq[String] = {
     val s = str + "$"
     val n = s.length
 
--- codeforces/src/test/scala/com/codeforces/contest/edu/suffix_array/SuffixArraySpec.scala
+++ codeforces/src/test/scala/com/codeforces/contest/edu/suffix_array/SuffixArraySpec.scala
@@ -3,10 +3,14 @@ package com.codeforces.contest.edu.suffix_array
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import com.codeforces.edu.suffix_array.SuffixArray.suffixes
+import com.codeforces.edu.suffix_array.SuffixArray._
 
 class SuffixArraySpec extends AnyFlatSpec with Matchers {
-  "'ababba'" should "have correct suffixes" in {
-    suffixes("ababba") should equal("ababba".tails.toList.sorted)
+  "'ababba'" should "have correct suffixes 1" in {
+    suffixes1("ababba") should equal("ababba".tails.toList.sorted)
+  }
+
+  "'ababba'" should "have correct suffixes 2" in {
+    suffixes2("ababba") should equal("ababba".tails.toList.sorted)
   }
 }

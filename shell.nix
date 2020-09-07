07d56199b27ba6ee9976254ee8267e3985ae8da8
Ivan Dyachenko <vandyachen@gmail.com>
2020-09-07 16:53:22 +0300

    Update nix-shell environment

--- shell.nix
+++ shell.nix
@@ -4,18 +4,8 @@ pkgs.stdenv.mkDerivation rec {
   name = "development-env";
   env = pkgs.buildEnv { name = name; paths = buildInputs; };
   buildInputs =
-    let
-      texlive-custom = pkgs.texlive.combine {
-        inherit (pkgs.texlive) scheme-basic
-          # Needed on top of scheme-basic
-          latexmk ulem capt-of wrapfig metafont;
-      };
-    in
       [
         # Development
         pkgs.metals-emacs
-        
-        # Documentation
-        pkgs.ditaa texlive-custom
       ];
 }

6d5a1a3bab2db684af43dee82db1314520032f32
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-10-05 01:08:31 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -4,9 +4,19 @@ pkgs.stdenv.mkDerivation rec {
   name = "development-env";
   env = pkgs.buildEnv { name = name; paths = buildInputs; };
   buildInputs =
+    let
+      texliveCustom = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-basic
+          # Needed on top of scheme-basic
+          latexmk wrapfig ulem capt-of lh metafont cyrillic babel-russian;
+      };
+    in
       [
         # Development
-        pkgs.emacsUnstable
         pkgs.metals
+        pkgs.emacsUnstable
+
+        # Documentation
+        pkgs.ditaa texliveCustom
       ];
 }

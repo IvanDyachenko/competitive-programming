301cb246fdc4b99b4581d71a575ef6ed7e04b5b2
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-10-09 00:06:13 +0300

    Codeforces Round #746 (Div. 2) https://codeforces.com/contest/1592

--- codeforces/src/test/scala/com/codeforces/contest/round746div2/ASuite.scala
+++ codeforces/src/test/scala/com/codeforces/contest/round746div2/ASuite.scala
@@ -5,15 +5,15 @@ import A._
 
 class ASuite extends FunSuite {
 
-  test("Codeforces | Round #745 | Div. 2 | A | 1 test case") {
+  test("Codeforces | Round #746 | Div. 2 | A | 1 test case") {
     assertEquals(gamerHemose(Array(3, 7), 4), 1)
   }
 
-  test("Codeforces | Round #745 | Div. 3 | A | 2 test case") {
+  test("Codeforces | Round #746 | Div. 2 | A | 2 test case") {
     assertEquals(gamerHemose(Array(4, 2), 6), 2)
   }
 
-  test("Codeforces | Round #745 | Div. 3 | A | 3 test case") {
+  test("Codeforces | Round #746 | Div. 2 | A | 3 test case") {
     assertEquals(gamerHemose(Array(2, 1, 7), 11), 3)
   }
 
--- codeforces/src/test/scala/com/codeforces/contest/round746div2/BSuite.scala
+++ codeforces/src/test/scala/com/codeforces/contest/round746div2/BSuite.scala
@@ -5,27 +5,27 @@ import B._
 
 class BSuite extends FunSuite {
 
-  test("Codeforces | Round #745 | Div. 2 | B | 1 test case") {
+  test("Codeforces | Round #746 | Div. 2 | B | 1 test case") {
     assert(!hemoseShopping(Array(3, 2, 1), 3))
   }
 
-  test("Codeforces | Round #745 | Div. 3 | B | 2 test case") {
+  test("Codeforces | Round #746 | Div. 2 | B | 2 test case") {
     assert(hemoseShopping(Array(1, 2, 3, 4), 3))
   }
 
-  test("Codeforces | Round #745 | Div. 3 | B | 3 test case") {
+  test("Codeforces | Round #746 | Div. 2 | B | 3 test case") {
     assert(hemoseShopping(Array(5, 1, 2, 3, 4), 2))
   }
 
-  test("Codeforces | Round #745 | Div. 2 | B | 4 test case") {
+  test("Codeforces | Round #746 | Div. 2 | B | 4 test case") {
     assert(hemoseShopping(Array(1, 2, 3, 4, 4), 4))
   }
 
-  test("Codeforces | Round #745 | Div. 2 | B | 5 test case") {
+  test("Codeforces | Round #746 | Div. 2 | B | 5 test case") {
     assert(hemoseShopping(Array(4, 3, 2, 1, 0), 3))
   }
 
-  test("Codeforces | Round #745 | Div. 2 | B | 6 test case") {
+  test("Codeforces | Round #746 | Div. 2 | B | 6 test case") {
     assert(!hemoseShopping(Array(4, 3, 2, 1), 3))
   }
 

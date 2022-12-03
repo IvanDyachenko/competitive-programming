8b92d70c5d10a724a442e7e014a998af90c3198e
Ivan Dyachenko <ivandyach@yandex-team.ru>
2022-12-03 22:47:14 +0400

    Use `pkgs.mkShell` instead of `pkgs.mkDerivation`

--- shell.nix
+++ shell.nix
@@ -1,23 +1,27 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
-pkgs.stdenv.mkDerivation rec {
-  name = "development-env";
-  env = pkgs.buildEnv { name = name; paths = buildInputs; };
-  buildInputs =
-    let
-      texliveCustom = pkgs.texlive.combine {
-        inherit (pkgs.texlive) scheme-basic
-          # Needed on top of scheme-basic
-          latexmk wrapfig ulem capt-of lh metafont cyrillic babel-russian;
-      };
-    in
-      [
-        # Development
-        pkgs.sbt
-        pkgs.metals
-        pkgs.scala-cli
+let
+  texliveCustom = pkgs.texlive.combine {
+    inherit (pkgs.texlive) scheme-basic
+      # Needed on top of scheme-basic
+      latexmk wrapfig ulem capt-of lh metafont cyrillic babel-russian;
+  };
+in
+pkgs.mkShell {
+  name = "competitive-programming";
 
-        # Documentation
-        pkgs.ditaa texliveCustom
-      ];
+  buildInputs = [
+    # Development
+    pkgs.adoptopenjdk-hotspot-bin-16
+    pkgs.sbt
+    pkgs.metals
+    pkgs.scala-cli
+
+    # Documentation
+    pkgs.ditaa texliveCustom
+  ];
+
+  shellHook = ''
+    export JAVA_HOME="${pkgs.adoptopenjdk-hotspot-bin-16}"
+  '';
 }

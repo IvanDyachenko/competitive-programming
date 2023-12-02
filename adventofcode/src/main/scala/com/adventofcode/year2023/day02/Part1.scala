26722e95e118a21920faad51ee766dbc8e148cc6
Ivan Dyachenko <ivandyach@yandex-team.ru>
2023-12-02 17:12:50 +0300

    Advent of Code 2023 [+] Day 2: Cube Conundrum, https://adventofcode.com/2023/day/2

---
+++ adventofcode/src/main/scala/com/adventofcode/year2023/day02/Part1.scala
@@ -0,0 +1,47 @@
+package com.adventofcode.year2023.day02
+
+/** Day 2: Cube Conundrum
+  *   - https://adventofcode.com/2023/day/2
+  */
+object Part1 extends App {
+
+  final case class Game(id: Int, red: Int = 0, blue: Int = 0, green: Int = 0) { self =>
+
+    def union: Game => Game = { case Game(_, red, blue, green) =>
+      Game(self.id, red max self.red, blue max self.blue, green max self.green)
+    }
+
+    def possible: Boolean =
+      red <= 12 && green <= 13 && blue <= 14
+  }
+
+  object Game {
+
+    def apply(input: String): Game = input match {
+      case s"Game $index: $games" =>
+        val id = index.toIntOption.getOrElse(0)
+
+        games
+          .split(';')
+          .foldLeft(Game(id)) { (game, input) =>
+            val games = input.split(',').toList.map(_.trim).map {
+              case s"$count red"   => Game(id, red = count.toIntOption.getOrElse(0))
+              case s"$count blue"  => Game(id, blue = count.toIntOption.getOrElse(0))
+              case s"$count green" => Game(id, green = count.toIntOption.getOrElse(0))
+              case _               => Game(id)
+            }
+
+            (game :: games).reduce(_ union _)
+          }
+      case _                      => Game(0)
+    }
+  }
+
+  val sources = scala.io.Source.fromResource("year2023/day02/input.txt").getLines().toList
+  val answer  = sources.map(Game.apply(_)).foldLeft(0L) {
+    case (state, game) if game.possible => state + game.id
+    case (state, _)                     => state
+  }
+
+  println(answer)
+}

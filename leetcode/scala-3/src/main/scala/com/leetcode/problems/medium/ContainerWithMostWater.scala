3009927dcef0c7513ef4fdfd26c62599e6475594
Ivan Dyachenko <inbox@ivandyachenko.ru>
2026-09-14 19:56:29 +0300

    Add Scala 3 solution for Container With Most Water

--- leetcode/scala-3/src/main/scala/com/leetcode/problems/medium/ContainerWithMostWater.scala
+++ leetcode/scala-3/src/main/scala/com/leetcode/problems/medium/ContainerWithMostWater.scala
@@ -1,8 +1,25 @@
 package com.leetcode.problems.medium
 
-// LeetCode #11: Container With Most Water
-// https://leetcode.com/problems/container-with-most-water/
-// https://www.hellointerview.com/learn/code/two-pointers/container-with-most-water
-object ContainerWithMostWater {
-  // Implement the solution here.
-}
+import scala.annotation.tailrec
+
+/** LeetCode #11: Container With Most Water
+  *
+  *   - https://leetcode.com/problems/container-with-most-water/
+  *   - https://www.hellointerview.com/learn/code/two-pointers/container-with-most-water
+  */
+object ContainerWithMostWater:
+  def maxArea(hs: Array[Int]): Int =
+    def area(l: Int, r: Int): Int = (r - l) * (hs(r) min hs(l))
+
+    @tailrec
+    def go(a: Option[Int] = None)(l: Int, r: Int): Option[Int] =
+      a match
+        case _ if l >= r                => a
+        case Some(a) if a >= area(l, r) =>
+          val (i, j) = if hs(l) < hs(r) then (l + 1, r) else (l, r - 1)
+          go(Some(a))(i, j)
+        case _ => go(Some(area(l, r)))(l, r)
+
+    go(None)(0, hs.length - 1) match
+      case Some(area) => area
+      case _          => 0
--- leetcode/scala-3/src/test/scala/com/leetcode/problems/medium/ContainerWithMostWaterSuite.scala
+++ leetcode/scala-3/src/test/scala/com/leetcode/problems/medium/ContainerWithMostWaterSuite.scala
@@ -1,12 +1,13 @@
 package com.leetcode.problems.medium
 
 import munit.FunSuite
+import ContainerWithMostWater.maxArea
 
 class ContainerWithMostWaterSuite extends FunSuite {
-  test("example") {
-    val actual   = 1 + 1
-    val expected = 2
-
-    assertEquals(actual, expected)
+  test("1") {
+    assertEquals(maxArea(Array(1, 1)), 1)
+  }
+  test("2") {
+    assertEquals(maxArea(Array(1, 8, 6, 2, 5, 4, 8, 3, 7)), 49)
   }
 }

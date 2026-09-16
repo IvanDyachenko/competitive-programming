adba0d6d53d82dd97cd8dd89cd64bfe6310924c9
Ivan Dyachenko <inbox@ivandyachenko.ru>
2026-09-16 21:37:21 +0300

    Implement ThreeSum in Scala and Rust

--- hellointerview/src/two_pointers/three_sum.rs
+++ hellointerview/src/two_pointers/three_sum.rs
@@ -1,7 +1,43 @@
 // https://www.hellointerview.com/learn/code/two-pointers/3-sum
 
-pub fn three_sum(_numbers: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!()
+pub fn three_sum(mut ns: Vec<i32>) -> Vec<Vec<i32>> {
+    ns.sort_unstable();
+    let len = ns.len();
+
+    let mut rs = Vec::new();
+
+    for i in 0..len {
+        if i > 0 && ns[i] == ns[i - 1] {
+            continue;
+        }
+
+        let (mut l, mut r) = (i + 1, len - 1);
+
+        while l < r && ns[i] <= 0 {
+            let s = ns[i] + ns[l] + ns[r];
+
+            if s < 0 {
+                l += 1;
+            } else if s > 0 {
+                r -= 1;
+            } else {
+                rs.push(vec![ns[i], ns[l], ns[r]]);
+
+                while l < r && ns[l] == ns[l + 1] {
+                    l += 1;
+                }
+
+                while l < r && ns[r] == ns[r - 1] {
+                    r -= 1;
+                }
+
+                l += 1;
+                r -= 1;
+            }
+        }
+    }
+
+    rs
 }
 
 #[cfg(test)]
--- leetcode/scala-3/src/main/scala/com/leetcode/problems/medium/ThreeSum.scala
+++ leetcode/scala-3/src/main/scala/com/leetcode/problems/medium/ThreeSum.scala
@@ -1,8 +1,37 @@
 package com.leetcode.problems.medium
 
+import scala.annotation.tailrec
+
 // LeetCode #15: 3Sum
 // https://leetcode.com/problems/3sum/
 // https://www.hellointerview.com/learn/code/two-pointers/3-sum
-object ThreeSum {
-  def threeSum(nums: Array[Int]): List[List[Int]] = ???
-}
+object ThreeSum:
+
+  def threeSum(nums: Array[Int]): List[List[Int]] =
+    val xs = nums.sorted
+
+    @tailrec
+    def go(i: Int, l: Int, r: Int)(rs: List[List[Int]]): List[List[Int]] =
+      () match
+        case _ if i + 2 >= xs.length || xs(i) > 0 =>
+          rs
+
+        case _ if l >= r || (i > 0 && xs(i) == xs(i - 1)) =>
+          go(i + 1, i + 2, xs.length - 1)(rs)
+
+        case _ if l > i + 1 && xs(l) == xs(l - 1) =>
+          go(i, l + 1, r)(rs)
+
+        case _ if r < xs.length - 1 && xs(r) == xs(r + 1) =>
+          go(i, l, r - 1)(rs)
+
+        case _ if xs(i) + xs(l) + xs(r) == 0 =>
+          go(i, l + 1, r - 1)(List(xs(i), xs(l), xs(r)) :: rs)
+
+        case _ if xs(i) + xs(l) + xs(r) > 0 =>
+          go(i, l, r - 1)(rs)
+
+        case _ =>
+          go(i, l + 1, r)(rs)
+
+    go(0, 1, xs.length - 1)(Nil)

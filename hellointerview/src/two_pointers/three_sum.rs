049bd21f1c7611e8972089524784bd71e009acd4
Ivan Dyachenko <inbox@ivandyachenko.ru>
2026-09-16 15:04:00 +0300

    Add LeetCode 3Sum examples for Rust and Scala

--- hellointerview/src/two_pointers/three_sum.rs
+++ hellointerview/src/two_pointers/three_sum.rs
@@ -20,4 +20,43 @@ mod tests {
 
         assert_eq!(actual, expected);
     }
+
+    #[test]
+    fn leetcode_example_1() {
+        let mut actual = three_sum(vec![-1, 0, 1, 2, -1, -4]);
+        let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+
+        for triplet in &mut actual {
+            triplet.sort_unstable();
+        }
+        actual.sort_unstable();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn leetcode_example_2() {
+        let mut actual = three_sum(vec![0, 1, 1]);
+        let expected = Vec::<Vec<i32>>::new();
+
+        for triplet in &mut actual {
+            triplet.sort_unstable();
+        }
+        actual.sort_unstable();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn leetcode_example_3() {
+        let mut actual = three_sum(vec![0, 0, 0]);
+        let expected = vec![vec![0, 0, 0]];
+
+        for triplet in &mut actual {
+            triplet.sort_unstable();
+        }
+        actual.sort_unstable();
+
+        assert_eq!(actual, expected);
+    }
 }
--- leetcode/scala-3/src/test/scala/com/leetcode/problems/medium/ThreeSumSuite.scala
+++ leetcode/scala-3/src/test/scala/com/leetcode/problems/medium/ThreeSumSuite.scala
@@ -10,4 +10,25 @@ class ThreeSumSuite extends FunSuite {
 
     assertEquals(actual, expected)
   }
+
+  test("LeetCode example 1") {
+    val actual   = ThreeSum.threeSum(Array(-1, 0, 1, 2, -1, -4)).map(_.sorted).sorted
+    val expected = List(List(-1, -1, 2), List(-1, 0, 1))
+
+    assertEquals(actual, expected)
+  }
+
+  test("LeetCode example 2") {
+    val actual   = ThreeSum.threeSum(Array(0, 1, 1)).map(_.sorted).sorted
+    val expected = List.empty[List[Int]]
+
+    assertEquals(actual, expected)
+  }
+
+  test("LeetCode example 3") {
+    val actual   = ThreeSum.threeSum(Array(0, 0, 0)).map(_.sorted).sorted
+    val expected = List(List(0, 0, 0))
+
+    assertEquals(actual, expected)
+  }
 }
